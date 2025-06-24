d86254ec8b217d88ecda5320faa1fcf479ca0b0c
losterberg3 <losterberg@ucsd.edu>
2025-06-24 11:19:07 -0700

    day2

---
+++ week1/week1_intro/build/CMakeFiles/newfile.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/newfile.dir/newfile.cpp.o"
+  "CMakeFiles/newfile.dir/newfile.cpp.o.d"
+  "newfile"
+  "newfile.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/newfile.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ week1/week1_intro/newfile.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, C++ from macOS, part 2!" << std::endl;
+    return 0;
+}
