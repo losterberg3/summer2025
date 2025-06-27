f4e3811134882174d6cdf993cae3a3b9f281acb1
losterberg3 <losterberg@ucsd.edu>
2025-06-26 21:38:54 -0700

    ignore

---
+++ week1/week1_ch1-chX/build/CMakeFiles/io.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/io.dir/ch9quiz.cpp.o"
+  "CMakeFiles/io.dir/ch9quiz.cpp.o.d"
+  "io"
+  "io.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/io.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
