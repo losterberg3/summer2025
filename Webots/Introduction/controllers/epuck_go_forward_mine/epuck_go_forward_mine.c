98835387ed66d40e607e5076f6efa9aa300638df
losterberg3 <losterberg@ucsd.edu>
2025-07-10 16:04:38 -0700

    week3day5

--- Webots/Introduction/controllers/epuck_go_forward_mine/epuck_go_forward_mine.c
+++ Webots/Introduction/controllers/epuck_go_forward_mine/epuck_go_forward_mine.c
@@ -22,7 +22,7 @@
 #define TIME_STEP 64
 
 #define MAX_SPEED 6.28
-
+ 
 int main(int argc, char **argv) {
   wb_robot_init();
 
@@ -33,8 +33,8 @@ int main(int argc, char **argv) {
   wb_motor_set_position(right_motor, INFINITY);
 
   // set up the motor speeds at 10% of the MAX_SPEED.
-  wb_motor_set_velocity(left_motor, 0.6 * MAX_SPEED);
-  wb_motor_set_velocity(right_motor, 0.1 * MAX_SPEED);
+  wb_motor_set_velocity(left_motor, 0.1 * MAX_SPEED);
+  wb_motor_set_velocity(right_motor, -0.1 * MAX_SPEED);
 
   while (wb_robot_step(TIME_STEP) != -1) {
   }
